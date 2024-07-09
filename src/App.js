98b49d760355c7ab5c17a381a3a202f41908004d
Ramya Vijayakumar <vramya57@gmail.com>
2024-07-09 22:28:07 +0400

    generate id

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
+const UUIDGenerator = () => {
+  const [uuids, setUuids] = useState([]);
+
+  const generateUUIDs = () => {
+    const newUuids = Array.from({ length: 1 }, () => uuidv4());
+    setUuids(newUuids);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button onClick={generateUUIDs}>Generate UUIDs</button>
+      <ul>
+        {uuids.map((uuid, index) => (
+          <li key={index}>{uuid}</li>
+        ))}
+      </ul>
     </div>
   );
-}
-
-export default App;
+};
+export default UUIDGenerator;
